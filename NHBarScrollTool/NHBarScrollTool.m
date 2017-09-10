01d468e6aa57a723d436a4bad75807fb8d7e7f5e
neghao <neghao@162.com>
2017-09-10 12:47:22 +0800

    去除多余的消息转发机制，增加RADME文件

--- NHBarScrollTool/NHBarScrollTool.m
+++ NHBarScrollTool/NHBarScrollTool.m
@@ -10,7 +10,7 @@
 #import "UIView+NHFrame.h"
 #import "MJRefresh.h"
 
-@interface NHBarScrollTool ()
+@interface NHBarScrollHelper : NSObject
 @property (nonatomic, weak  ) UIViewController   *currentConteroller;
 @property (nonatomic, weak  ) UITabBarController *tabBarController;
 @property (nonatomic, weak  ) UITabBar           *tabBar;
@@ -23,128 +23,23 @@
 @property (nonatomic, assign) CGFloat            tabBarOriginallY;
 @property (nonatomic, assign) CGFloat            navgationHeight;
 @property (nonatomic, assign) CGFloat            tabbarHeight;
-
-
 @end
 
 
-@implementation NHBarScrollTool
-
-+ (instancetype)BarScrollToolWithController:(UIViewController *)viewController
-                                 scrollView:(UIScrollView *)scrollView
-                              navigationBar:(__kindof UIView *)navigationBar
-                                     tabBar:(__kindof UIView *)tabBar {
-    
-    return [[[NHBarScrollTool alloc] init] BarScrollToolWithController:viewController
-                                                            scrollView:scrollView
-                                                         navigationBar:navigationBar
-                                                                tabBar:tabBar];
-
-}
-
-- (instancetype)BarScrollToolWithController:(UIViewController *)viewController
-                                 scrollView:(UIScrollView *)scrollView
-                              navigationBar:(__kindof UIView *)navigationBar
-                                     tabBar:(__kindof UIView *)tabBar {
-    
-    if (self == [super init]) {
-
-        _currentConteroller = viewController;
-        _tabBarController = viewController.tabBarController;
-        _navigationView = navigationBar ?: viewController.navigationController.navigationBar;
-        _tabBar = tabBar ?: viewController.tabBarController.tabBar;
-        _navBarOriginallY = _navigationView.top;
-        _tabBarOriginallY = _tabBar.top;
-        _navgationHeight = _navigationView.height + _navBarOriginallY;
-        _tabbarHeight = _tabBar.height;
-        _scrollView = scrollView;
-        
-    }
-    return self;
-}
-
-- (BOOL)respondsToSelector:(SEL)aSelector{
-    
-//    if ([super respondsToSelector:aSelector]) {
-//        return YES;
-//    }
-    for (id target in self.weakRefTargets) {
-        if (target && [target respondsToSelector:aSelector]) {
-            return YES;
-        }
-    }
-    
-    return NO;
-}
-
-- (NSMethodSignature *)methodSignatureForSelector:(SEL)aSelector{
-    NSMethodSignature *sig = [super methodSignatureForSelector:aSelector];
-    if (!sig) {
-        [_weakRefTargets compact];
-        for (id target in self.weakRefTargets) {
-            if ((sig = [target methodSignatureForSelector:aSelector])) {
-                break;
-            }
-        }
-    }
-    
-    return sig;
-}
-
-//转发方法调用给所有delegate
-- (void)forwardInvocation:(NSInvocation *)anInvocation{
-    for (id target in self.weakRefTargets) {
-        if ([target respondsToSelector:anInvocation.selector]) {
-            [anInvocation invokeWithTarget:target];
-        }
-    }
-}
-
-- (void)removeObserver:(id)delegateTag {
-    NSUInteger index = [self indexOfDelegate:delegateTag];
-    if (index != NSNotFound) {
-        [_weakRefTargets removePointerAtIndex:index];
-    }
-    [_weakRefTargets compact];
-}
-
-- (NSUInteger)indexOfDelegate:(id)delegate {
-    for (NSUInteger i = 0; i < _weakRefTargets.count; i++) {
-        if ([_weakRefTargets pointerAtIndex:i] == (__bridge void*)delegate) {
-            return i;
-        }
-    }
-    return NSNotFound;
-}
-
-- (void)setDelegateTargets:(NSArray *)delegateTargets {
-    _delegateTargets = delegateTargets;
-    self.weakRefTargets = [NSPointerArray weakObjectsPointerArray];
-    for (id delegate in delegateTargets) {
-        [self.weakRefTargets addPointer:(__bridge void * _Nullable)(delegate)];
-    }
-}
+@implementation NHBarScrollHelper
 
 #pragma mark - UIScrollViewDelegate
 - (void)scrollViewDidScroll:(UIScrollView *)scrollView {
     _scrollView = scrollView;
     
-    //    for (id target in self.weakRefTargets) {
-    //        if ([target respondsToSelector:_cmd]) {
-    //            [target scrollViewDidScroll:scrollView];
-    //        }
-    //    }
-    
     CGFloat scrollViewOffsetY = scrollView.contentOffset.y;
     _moveOffset = scrollViewOffsetY - _lastPointY;
-    NSLog(@"contentOffsetY:%f",scrollView.contentOffset.y);
+//    NSLog(@"contentOffsetY:%f",scrollView.contentOffset.y);
     
     
-    if (scrollViewOffsetY >= _navgationHeight) {
+//    if (scrollViewOffsetY >= _navgationHeight) {
 //        return;
-    }
-    
-   
+//    }
     
     //往下拽刷新的时候，结束掉
     if (scrollViewOffsetY <= -_scrollView.contentInset.top) {
@@ -189,7 +84,6 @@
 }
 
 
-
 #pragma updateBarFrame
 - (void)updatePageBarFrame {
     
@@ -198,7 +92,7 @@
         return;
     }
     [self updateNavigationBarFrame];
-
+    
     [self updateTabBarFrame];
     
     
@@ -208,8 +102,8 @@
 
 - (void)updateNavigationBarFrame {
     CGFloat pageBarOffsetY = _navigationView.top;
-    NSLog(@"%f--%f",_navigationView.top,_moveOffset);
-
+//    NSLog(@"%f--%f",_navigationView.top,_moveOffset);
+    
     if (_moveOffset > 0) {
         if (pageBarOffsetY <= -(_navgationHeight - _navBarOriginallY)) {
             _navigationView.top = -(_navgationHeight - _navBarOriginallY);
@@ -265,7 +159,7 @@
 
 - (void)updataTabBarAndNavigationBarFrame {
     
-//    CGFloat tabBarOffset = kScreenHeight - _tabBar.top;
+    //    CGFloat tabBarOffset = kScreenHeight - _tabBar.top;
     CGFloat scrollViewOffsetY = _scrollView.contentOffset.y;
     
     //    kNSLog(@"%f",_scrollView.contentOffset.y);
@@ -299,6 +193,125 @@
         _tabBarController.hidesBottomBarWhenPushed = YES;
         _navigationView.top = -_navgationHeight;
     }
+}
+
+@end
+
+
+@interface NHBarScrollTool ()
+//这个必须要用strong，不会导致无法释放问题
+@property (nonatomic, strong) NSPointerArray     *weakRefTargets;
+@property (nonatomic, strong) NHBarScrollHelper  *scrollHelper;
+
+@end
+
+
+@implementation NHBarScrollTool
+
++ (instancetype)BarScrollToolWithController:(UIViewController *)viewController
+                                 scrollView:(UIScrollView *)scrollView
+                              navigationBar:(__kindof UIView *)navigationBar
+                                     tabBar:(__kindof UIView *)tabBar {
+    
+    return [[[NHBarScrollTool alloc] init] BarScrollToolWithController:viewController
+                                                            scrollView:scrollView
+                                                         navigationBar:navigationBar
+                                                                tabBar:tabBar];
+
+}
+
+- (instancetype)BarScrollToolWithController:(UIViewController *)viewController
+                                 scrollView:(UIScrollView *)scrollView
+                              navigationBar:(__kindof UIView *)navigationBar
+                                     tabBar:(__kindof UIView *)tabBar {
+    
+    if (self == [super init]) {
+        
+        UIView *navBar = navigationBar ?: viewController.navigationController.navigationBar;
+        UITabBar *tBar = tabBar ?: viewController.tabBarController.tabBar;
+        self.scrollHelper.currentConteroller = viewController;
+        self.scrollHelper.tabBarController = viewController.tabBarController;
+        self.scrollHelper.navigationView = navBar;
+        self.scrollHelper.tabBar = tBar;
+        self.scrollHelper.navBarOriginallY = navBar.top;
+        self.scrollHelper.tabBarOriginallY = tBar.top;
+        self.scrollHelper.navgationHeight = navBar.height + navBar.top;
+        self.scrollHelper.tabbarHeight = tBar.height;
+        self.scrollHelper.scrollView = scrollView;
+        
+    }
+    return self;
+}
+
+- (NHBarScrollHelper *)scrollHelper {
+    if (!_scrollHelper) {
+        _scrollHelper = [[NHBarScrollHelper alloc] init];
+    }
+    return _scrollHelper;
+}
+
+- (BOOL)respondsToSelector:(SEL)aSelector{
+    if ([super respondsToSelector:aSelector]) {
+        return YES;
+    }
+    for (id target in self.weakRefTargets) {
+        if (target && [target respondsToSelector:aSelector]) {
+            return YES;
+        }
+    }
+    
+    return NO;
+}
+
+- (NSMethodSignature *)methodSignatureForSelector:(SEL)aSelector{
+    NSMethodSignature *sig = [super methodSignatureForSelector:aSelector];
+    if (!sig) {
+        [_weakRefTargets compact];
+        for (id target in self.weakRefTargets) {
+            if ((sig = [target methodSignatureForSelector:aSelector])) {
+                break;
+            }
+        }
+    }
+    
+    return sig;
+}
+
+//转发方法调用给所有delegate
+- (void)forwardInvocation:(NSInvocation *)anInvocation{
+    for (id target in self.weakRefTargets) {
+        if ([target respondsToSelector:anInvocation.selector]) {
+            [anInvocation invokeWithTarget:target];
+        }
+    }
+}
+
+- (void)removeObserver:(id)delegateTag {
+    NSUInteger index = [self indexOfDelegate:delegateTag];
+    if (index != NSNotFound) {
+        [_weakRefTargets removePointerAtIndex:index];
+    }
+    [_weakRefTargets compact];
+}
+
+- (NSUInteger)indexOfDelegate:(id)delegate {
+    for (NSUInteger i = 0; i < _weakRefTargets.count; i++) {
+        if ([_weakRefTargets pointerAtIndex:i] == (__bridge void*)delegate) {
+            return i;
+        }
+    }
+    return NSNotFound;
+}
+
+//保存代理
+- (void)setDelegateTargets:(NSArray *)delegateTargets {
+    _delegateTargets = delegateTargets;
+    self.weakRefTargets = [NSPointerArray weakObjectsPointerArray];
+    for (id delegate in delegateTargets) {
+        [self.weakRefTargets addPointer:(__bridge void * _Nullable)(delegate)];
+    }
+    [self.weakRefTargets addPointer:(__bridge void * _Nullable)(self.scrollHelper)];
+
 }
 
 
---
+++ README.md
@@ -0,0 +1,55 @@
+## 导航栏、工具栏一键跟随tableview的滚动来控制显示和隐藏
+
+#### 使用方法：
+把相关类拖入工程(如下图)：
+![](https://github.com/neghao/NHBarScrollTool/blob/master/filepath.png)
+你也可以使用cocoapod管理：
+`pod "NHBarScrollTool"`
+
+> 1，添加NHBarScrollTool的代理对象，是个数组，可以添加多个对象，被添加进去的对象都可以接收到scrollView的代理方法
+>  barScrollTool.delegateTargets = @[ self ];
+> 2，把tableView的代理设置为：NHBarScrollTool/Users/neghao/Library/Containers/com.tencent.qq/Data/Library/Application Support/QQ/Users/403700907/QQ/Temp.db/BEE309A4-32B0-49BD-B7F9-F7AB8D9EDEF9.png
+
+>  self.tableView.delegate = self.barScrollTool;
+> 3，一定要先设置`_barScrollTool.delegateTargets = ...`再设置self.tableView.delegate = barScrollTool
+
+
+
+```
+@interface UIViewController ()
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (nonatomic, strong) NHBarScrollTool *barScrollTool;
+@end
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    [self setupTableView];
+}
+
+- (void)setupTableView {
+
+    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"cell"];
+    self.tableView.dataSource = self;
+    self.tableView.delegate = self.barScrollTool;
+    self.tableView.contentInset = UIEdgeInsetsMake(64, 0, 0, 0);
+    self.tableView.scrollIndicatorInsets = UIEdgeInsetsMake(64, 0, 0, 0);
+}
+
+- (NHBarScrollTool *)barScrollTool {
+    if (!_barScrollTool) {
+        _barScrollTool = [NHBarScrollTool BarScrollToolWithController:self
+                                                           scrollView:_tableView
+                                                        navigationBar:nil
+                                                               tabBar:nil];
+        _barScrollTool.delegateTargets = @[ self ];
+    }
+    return _barScrollTool;
+}
+
+```
+
+
+比较忙，没写太细节，代码里面的注释也不是很全，欢迎使用吐槽。
+
+
